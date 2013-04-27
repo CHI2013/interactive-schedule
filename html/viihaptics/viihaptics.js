b001d0f3d35240b5c8c1f310aee6ea8e99f18036
Clemens Nylandsted Klokmose <clemens@cs.au.dk>
2013-04-27 16:04:24 +0200

    viihaptics integration

--- html/viihaptics/viihaptics.js
+++ html/viihaptics/viihaptics.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
     var socket = io.connect("http://" + window.location.hostname, {
         port: 8000
     });
-   
+    
     socket.on('finger', function(data) {
-        console.log(data);
+        moveCircle(data.x,data.y,data.z);
     });
     
 });
+
+function moveCircle(x,y,z) {
+    $("#pointer").css("left",x).css("top",y);
+}
