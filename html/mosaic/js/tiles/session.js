07e3d9a9e4670656e3652ca2b00ad96f1c872e51
Clemens Nylandsted Klokmose <clemens@cs.au.dk>
2013-04-28 11:28:18 +0200

    viihaptics integration

--- html/mosaic/js/tiles/session.js
+++ html/mosaic/js/tiles/session.js
@@ -133,7 +133,7 @@ var tagCloudTimeStamp;
 
 function checkHover() {
     var now = new Date();
-    if (tagCloudTimeStamp != undefined && (now.getTime() - tagCloudTimeStamp.getTime()) > 10000) {
+    if (tagCloudTimeStamp != undefined && (now.getTime() - tagCloudTimeStamp.getTime()) > 5000) {
         $('#tagcloud').hide();
         $('#submissions').hide();
         $('#action').show();
